acdeb407708ea9fae755ed314d1e506e2dffee66
Juan Carrion <hola@juancrrn.io>
2022-04-11 21:10:56 +0200

    Formatted face locations response

--- app/mc_face_detection_ms/domain/worker/face_detection_worker.py
+++ app/mc_face_detection_ms/domain/worker/face_detection_worker.py
@@ -1,3 +1,4 @@
+from curses import raw
 import os, cv2, dlib, face_recognition
 from werkzeug.exceptions import NotFound, InternalServerError
 from config import DATA_DIR
@@ -29,6 +30,22 @@ class FaceDetectionWorker:
         self.full_path = self.get_full_data_path()
         self.assert_file_exists()
 
+    @staticmethod
+    def transform_face_location(id: int, face_location: list) -> dict:
+        return {
+            "id": id,
+            "edges": {
+                "top":      face_location[0],
+                "right":    face_location[1],
+                "bottom":   face_location[2],
+                "left":     face_location[3]
+            }
+        }
+
     def get_face_locations(self) -> list:
         image = face_recognition.load_image_file(self.full_path)
-        return face_recognition.face_locations(image, model = 'cnn')
+        raw_face_locations = face_recognition.face_locations(image, model = 'cnn')
+        face_locations = []
+        for i in range(0, len(raw_face_locations)):
+            face_locations.append(FaceDetectionWorker.transform_face_location(i + 1, raw_face_locations[i]))
+        return face_locations
